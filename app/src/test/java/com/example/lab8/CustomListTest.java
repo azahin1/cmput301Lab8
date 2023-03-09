3ec32364b6384d03b2257a57ae736515a51f4c4c
azahin1 <azahin1@ualberta.ca>
2023-03-09 15:05:10 -0700

    test code for delete city in custom list

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -3,6 +3,7 @@ package com.example.lab8;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
@@ -35,4 +36,27 @@ public class CustomListTest {
         list.addCity(city);
         assertTrue(list.hasCity(city));
     }
+
+    @Test
+    void testDelete() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        City city = new City("Edmonton", "AB");
+        assertEquals(list.getCount(), listSize); // nothing has happened
+        list.addCity(city);
+        assertEquals(list.getCount(), listSize + 1); // add one list
+        list.delCity(city);
+        assertEquals(list.getCount(), listSize); // return to original
+        assertFalse(list.hasCity(city));
+    }
+
+    @Test
+    void testDeleteException() {
+        list = MockCityList();
+        City city = new City("Edmonton", "AB");
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.delCity(city); // edmonton not in list
+        });
+    }
+
 }
